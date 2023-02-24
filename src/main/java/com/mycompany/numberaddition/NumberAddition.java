840e2e114d25599895db1a4c88093e4d1883893d
Oliver Wolff <oliver.wolff@mrjaffesclass.com>
2023-02-24 12:43:09 -0800

    first commit

---
+++ src/main/java/com/mycompany/numberaddition/NumberAddition.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.numberaddition;
+
+/**
+ *
+ * @author student
+ */
+public class NumberAddition {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
